3befe9f55aa9e2ff3b2ab921251f8eb2ef807d48
Vani Saxena <71544568+vanisaxena@users.noreply.github.com>
2023-10-21 01:02:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/v1/product', productRoutes);
 
 
 //rest api
-app.use('*',function(req,res){
+app.get('*',function(req,res){
     res.sendFile(path.join(__dirname, './client/build/index.html'))
 });
 
