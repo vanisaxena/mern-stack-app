671e8558e17cd9cf2dd9688de1e6cb6201525a85
Vani Saxena <71544568+vanisaxena@users.noreply.github.com>
2023-10-21 14:50:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
-import cors from "cors"
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from  'url;
+
 
  // configure dotenv
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 //Database connect
 connectDB();
@@ -27,7 +27,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, './client/build ')));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use('/api/v1/auth', authRoutes);
